11395f246b0b8596eb55b9ba09a12e188af9fe60
Illuminubby <illuminubby@gmail.com>
2023-03-07 03:08:46 -0500

    add webhooks for creating a new shot entry in db, calculating the stability for points in a shot, and creating an image from stabilities in db

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 from flask import Flask, jsonify, request, make_response
 from flask_restful import Resource, Api
 
+
+
 import Shot
+import Picture
+from db import createNewShotEntry, fetchShotById, updateStabilitiesById, fetchStabilitiesById
 
 app = Flask(__name__)
 
 p = "Here We Go!"
+
+ROYGBIV = [ 
+    [148, 0, 211, 255], 
+    [75, 0, 130, 255], 
+    [0, 0, 255, 255],
+    [0, 255, 0, 255], 
+    [255, 255, 0, 255], 
+    [255, 127, 0, 255], 
+    [255, 0 , 0, 255] 
+]
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -33,17 +47,56 @@ def testBook(testVar=None):
         abort(400)
 
 @app.route('/createNewShot', methods=['POST'])
-def createNewShot():
+async def createNewShot():
 
     if request.method == 'POST':
         if (request.json['name'] != None and request.json['limits'] != None and request.json['reso'] != None and request.json['maxStability'] != None):
 
             newShot = Shot.Shot(request.json['name'], request.json['limits'], request.json['reso'], request.json['maxStability'])
 
-            print(newShot.getName())
-            print(newShot.getLimits())
-            print(newShot.getReso())
-            print(newShot.getMaxStability())
+            await createNewShotEntry(request.json['name'], request.json['limits'], request.json['reso'], request.json['maxStability'])
+
+            return 'success', 200
+        else:
+            abort(400)
+    else:
+        abort(400)
+
+@app.route('/calculateStabilitiesById', methods=['POST'])
+async def calculateStabilitiesById():
+
+    if request.method == 'POST':
+        if (request.json['id'] != None and request.json['max_stability'] != None and request.json['z_value'] != None):
+
+            #newShot = Shot.Shot(request.json['name'], request.json['limits'], request.json['reso'], request.json['maxStability'])
+
+            shot_props = await fetchShotById( request.json['id'] )
+
+            positionalVals = await Shot.createPointsList( shot_props['limits'], shot_props['reso'] )
+
+            stabilityVals = await Shot.checkStability( positionalVals, request.json['z_value'], request.json['max_stability'] )
+
+            await updateStabilitiesById( request.json['id'], stabilityVals )
+
+            return 'success', 200
+        else:
+            abort(400)
+    else:
+        abort(400)
+
+@app.route('/createPicFromStabilities', methods=['POST'])
+async def createPicFromStabilities():
+
+    if request.method == 'POST':
+        if (request.json['id'] != None):
+
+            shot_props = await fetchShotById( request.json['id'] )
+
+            stabilityVals = await fetchStabilitiesById( request.json['id'] )
+            #print(stabilityVals['stabilities'][:50])
+            stylizedArray = await Picture.stylizeStabiliesForPic( stabilityVals['stabilities'], ROYGBIV, shot_props['reso'], stabilityVals['max_stability'] )
+            #print(stylizedArray[:25])
+            pic = await Picture.generatePicFromStylizedPoints( stylizedArray, shot_props['reso'],  )
 
             return 'success', 200
         else:
