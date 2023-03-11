039d823c3c0fc428703d8c44162c6d479edb0a5d
Illuminubby <illuminubby@gmail.com>
2023-03-11 04:26:11 -0500

    add method to fetch info on all stored shots, changed createNewShotEntry to return the id of created shot

--- src/db.py
+++ src/db.py
@@ -7,9 +7,11 @@ config = dotenv_values(".env")
 async def createNewShotEntry(name, limits, reso, maxStability):
     name = '\'' + name + '\''
     cur = conn.cursor()
-    query = """INSERT INTO public.shot (name, limits, reso, max_stability) VALUES (%s, %s, %s, %d)""" % (name, "ARRAY " + str(limits), "ARRAY " + str(reso), maxStability)
+    query = """INSERT INTO public.shot (name, limits, reso, max_stability) VALUES (%s, %s, %s, %d) RETURNING id""" % (name, "ARRAY " + str(limits), "ARRAY " + str(reso), maxStability)
     cur.execute( query )
+    newId = cur.fetchone()[0]
     conn.commit()
+    return newId
 
 async def updateStabilitiesById(id, stabilities):
     cur = conn.cursor()
@@ -34,10 +36,28 @@ async def fetchStabilitiesById(id):
 
 async def fetchShotForCopy(id):
     cur = conn.cursor()
-    query="""SELECT name, limits, reso, max_stability FROM public.shot WHERE id = %d""" % ( id )
+    query="""SELECT name, limits, reso, max_stability FROM public.shot WHERE id = %d""" % ( int(id) )
     cur.execute( query )
     rows = cur.fetchall()
     return { "id": id, "name": rows[0][0], "limits": rows[0][1], "reso": rows[0][2], "max_stability": rows[0][3] }
+
+async def fetchAllShotsInfo():
+    cur = conn.cursor()
+    query="""SELECT id, name, limits, reso FROM public.shot"""
+    cur.execute( query )
+    rows = cur.fetchall()
+    print(rows)
+    theRows = []
+    for row in rows:
+        tempLimits = row[2].copy()
+        for t in range(len(tempLimits)):
+            tempLimits[t] = round( tempLimits[t], 3 )
+        tempRow = {"id": row[0], "name": row[1], "limits": tempLimits, "reso": row[3]}
+        theRows.append(tempRow)
+
+    return { "results": theRows }
+
+
 
 # terminal command to start db
 # psql -h localhost -p 5432 -U postgres -f db.sql
